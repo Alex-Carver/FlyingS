20bd66207902f8ec707633512e91b669da1151ad
Alex <Alex-Carver@.noreply.github.com>
2018-11-21 15:58:13 -0600

    new

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
 $('.carousel').carousel({
-  interval: 2000
-})
-
-$("#button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#section-2").offset().top
-    }, 2000);
+  interval: 3000
 });
+
